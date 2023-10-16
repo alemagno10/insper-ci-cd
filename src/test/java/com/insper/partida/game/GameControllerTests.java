0b1aa6d901671993fd89867c9589490ec197b348
alemagno10 <alemagno03@gmail.com>
2023-10-16 15:27:07 -0300

    add: GameControllerTest getlistgames

--- src/test/java/com/insper/partida/game/GameControllerTests.java
+++ src/test/java/com/insper/partida/game/GameControllerTests.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -27,6 +28,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.insper.partida.equipe.dto.TeamReturnDTO;
 import com.insper.partida.game.dto.EditGameDTO;
 import com.insper.partida.game.dto.GameReturnDTO;
 
@@ -45,6 +47,7 @@ public class GameControllerTests {
     void setup()  {
         this.mockMvc = MockMvcBuilders
                 .standaloneSetup(gameController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .build();
     }
 
@@ -66,39 +69,40 @@ public class GameControllerTests {
         Assertions.assertEquals(om.writeValueAsString(game), resp);
     }
 
-    // @Test
-    // void getListGames() throws Exception {
-    //     List<GameReturnDTO> games = new ArrayList<>();
+    @Test
+    void getListGames() throws Exception {
+        List<GameReturnDTO> games = new ArrayList<>();
 
-    //     TeamReturnDTO team1 = new TeamReturnDTO();
-    //     team1.setIdentifier("Kansas");
-    //     TeamReturnDTO team2 = new TeamReturnDTO();
-    //     team2.setIdentifier("Dallas");
+        TeamReturnDTO team1 = new TeamReturnDTO();
+        team1.setIdentifier("Kansas");
+        TeamReturnDTO team2 = new TeamReturnDTO();
+        team2.setIdentifier("Dallas");
 
-    //     GameReturnDTO game1 = new GameReturnDTO();
-    //     game1.setIdentifier("game1");
-    //     game1.setHome(team1);
-    //     game1.setAway(team2);
-    //     games.add(game1);
+        GameReturnDTO game1 = new GameReturnDTO();
+        game1.setIdentifier("game1");
+        game1.setHome(team1);
+        game1.setAway(team2);
+        game1.setAttendance(10000);
+        games.add(game1);
 
-    //     PageRequest paginacao = PageRequest.of(0, 10);
-    //     Mockito.when(gameService.listGames(Mockito.any(String.class), Mockito.any(String.class), Mockito.any(Integer.class), Mockito.eq(paginacao)))
-    //             .thenReturn((new PageImpl<>(games, paginacao, games.size())));
+        PageRequest paginacao = PageRequest.of(0, 10);
+        Mockito.when(gameService.listGames(Mockito.anyString(), Mockito.anyString(), Mockito.anyInt(), Mockito.any()))
+                .thenReturn((new PageImpl<>(games, paginacao, games.size())));
 
-    //     MvcResult result = mockMvc
-    //         .perform(MockMvcRequestBuilders.get("/game")
-    //             .param("home", "Kansas")
-    //             .param("away", "Dallas")
-    //             .param("page", "0") // Número da página desejada
-    //             .param("size", "10"))
-    //         .andExpect(MockMvcResultMatchers.status().isOk())
-    //         .andReturn();
+        MvcResult result = mockMvc
+            .perform(MockMvcRequestBuilders.get("/game")
+                .param("home", "Kansas")
+                .param("away", "Dallas")
+                .param("attendance", "10000"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andReturn();
 
-    //     ObjectMapper om = new ObjectMapper();
+        ObjectMapper om = new ObjectMapper();
 
-    //     String resp = result.getResponse().getContentAsString();
-    //     Assertions.assertEquals(om.writeValueAsString(games), resp);
-    // }
+        Page<GameReturnDTO> gamePage = new PageImpl<>(games, paginacao, 2);
+        String resp = result.getResponse().getContentAsString();
+        Assertions.assertEquals(om.writeValueAsString(gamePage), resp);
+    }
 
     @Test
     void testChangeGame() throws Exception {
